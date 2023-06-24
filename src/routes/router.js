b11b222c56f104468ee0c6dfb64933695415bf94
lana2-2karla <allanakarla1702@gmail.com>
2023-06-24 15:35:31 -0300

    :sparkles: feat: route '/smartphone' - PUT

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,10 +1,12 @@
 const { Router } = require('express')
-const { createSmartphoneController, getAllSmartphoneController } = require('../controllers/smartphoneController')
+const { createSmartphoneController, getAllSmartphoneController, updateSmartphoneController } = require('../controllers/smartphoneController')
 
 const router = Router()
 
 router.post('/smartphone', createSmartphoneController)
 
 router.get('/smartphone', getAllSmartphoneController)
+
+router.put('/smartphone', updateSmartphoneController)
 
 module.exports = router
