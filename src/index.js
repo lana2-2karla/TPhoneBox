688bff8e4d6ec3766a7ddaffea44c38939cd1947
lana2-2karla <allanakarla1702@gmail.com>
2023-06-24 13:00:40 -0300

    :sparkles: feat: mongoDB connection and import router - index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
+const router = require('./routes/router')
 
 app.use(express.json())
+
+// mongoDB connection
+const conn = require('../mongodb/conn')
+conn()
+
+app.use(router)
 
 app.listen(process.env.APP_PORT, () => console.log(`ouvindo na porta ${process.env.APP_PORT}`))
