73826c2f3c6c58092f09c2b7456224b66cb4b710
lana2-2karla <allanakarla1702@gmail.com>
2023-06-26 17:24:29 -0300

    :ok_hand: style: indentation

--- testes/meus_testes.py
+++ testes/meus_testes.py
@@ -9,22 +9,24 @@ navegador = webdriver.Chrome(service=servico)
 
 class PythonTPhoneBoxSearch(unittest.TestCase):
 
-    def setUp(self):
-        self.driver = navegador
+  def setUp(self):
+    self.driver = navegador
 
-    def test_search_button_header_displayed(self):
-        # Navegação para o site
-        self.driver.get('http://localhost:5173')
+  def test_search_button_header_displayed(self):
+    # Navegação para o site
+    self.driver.get('http://localhost:5173')
 
-        # Verificação do primeiro botão no Header
-        search_first__button = self.driver.find_element('xpath', '//*[@id="app"]/header/div/div/div[2]/button[1]')
-        self.assertTrue(search_first__button.is_displayed())
+    # Verificação do primeiro botão no Header
+    search_first__button = self.driver.find_element('xpath', '//*[@id="app"]/header/div/div/div[2]/button[1]')
+    self.assertTrue(search_first__button.is_displayed())
 
-    def tearDown(self):
-        # Fechamento do navegador
-        self.driver.quit()
+  
+
+  def tearDown(self):
+    # Fechamento do navegador
+    self.driver.quit()
 
 if __name__ == '__main__':
-    unittest.main()
+  unittest.main()
 
 
