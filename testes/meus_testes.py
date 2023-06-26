526d6961ccacd14f1e6313ec01554f7af653b646
lana2-2karla <allanakarla1702@gmail.com>
2023-06-26 18:22:19 -0300

    :white_check_mark: test: second button in header

--- testes/meus_testes.py
+++ testes/meus_testes.py
@@ -1,26 +1,32 @@
 import unittest
+import time
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 
-
 servico = Service(ChromeDriverManager().install())
-navegador = webdriver.Chrome(service=servico)
 
 class PythonTPhoneBoxSearch(unittest.TestCase):
 
   def setUp(self):
+    navegador = webdriver.Chrome(service=servico)
     self.driver = navegador
+    self.driver.maximize_window()
+    self.driver.get('http://localhost:5173')
 
   def test_search_button_header_displayed(self):
     # Navegação para o site
-    self.driver.get('http://localhost:5173')
 
     # Verificação do primeiro botão no Header
-    search_first__button = self.driver.find_element('xpath', '//*[@id="app"]/header/div/div/div[2]/button[1]')
-    self.assertTrue(search_first__button.is_displayed())
+    first_button = self.driver.find_element('xpath', '//*[@id="app"]/header/div/div/div[2]/button[1]')
+    self.assertTrue(first_button.is_displayed())
 
-  
+  def test_second_button_header_displayed(self):
+
+    # Verificação de outro botão no header
+    second_button = self.driver.find_element('xpath', '//*[@id="app"]/header/div/div/div[2]/button[2]')
+    self.assertTrue(second_button.is_displayed())
+
 
   def tearDown(self):
     # Fechamento do navegador
